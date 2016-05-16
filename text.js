97156701731f883ab0acccad6dc755019ed4028b
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2016-05-16 11:29:53 -0400

    added Detroit facts

--- text.js
+++ text.js
@@ -1,4 +1,10 @@
-var text = ['quote 1', 'quote 2', 'quote 3'];
+var text = [
+	'Detroit is the largest city in the state of Michigan.', 
+	'Detroit is the birthplace of Motown and electronic music.', 
+	'The Tigers, Lions, Red Wings, and Pistons are all Detroit sports teams.', 
+	"Detroit houses the largest and oldest farmer's market in the United States.", 
+	"Belle Isle Park is larger than New York's Cental Park"
+];
 
 function randomizeText() {
 	return text[Math.floor(Math.random() * text.length)];
